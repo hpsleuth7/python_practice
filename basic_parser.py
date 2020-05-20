38e5f4b83146ea09cace2461a90cfdc2bef48a5b
hpsleuth7 <murphy.c.austin@gmail.com>
2020-05-20 16:28:53 -0400

    create sample html and write basic python request

--- basic_parser.py
+++ basic_parser.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
-
+import requests
 
 def main():
 
-  soup = BeautifulSoup(page.content, 'html.parser')
+  #soup = BeautifulSoup(page.content, 'html.parser')
+  
+  page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+  print(type(page))
+  print(page.status_code)
 
 
 if __name__ == '__main__':
