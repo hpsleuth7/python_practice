7ff8a80f229d184c6cef1a0c1659861caa8fc7a7
hpsleuth7 <murphy.c.austin@gmail.com>
2020-06-09 16:29:44 -0400

    wikipedia search page exploration

--- selenium-practice.py
+++ selenium-practice.py
@@ -1,10 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 def main():
 
   driver = webdriver.Firefox()
   
   driver.get('https://wikipedia.com')
+ 
+  # pull out 'select language' element  
+  search_language = driver.find_element_by_id('searchLanguage') 
+  
+  # command driver to select Latin 
+  Select(search_language).select_by_visible_text('Latina')
+  
+  # pull out input element for search 
+  search_input = driver.find_element_by_id('searchInput')
+  
+  # command driver to input 'Marcus Aurelius' into search element 
+  search_input.send_keys('Marcus Aurelius')
+  
   
   driver.quit()
   
